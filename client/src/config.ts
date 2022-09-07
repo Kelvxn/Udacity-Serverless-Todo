cb3dfd8249727966103f52b629c7e13a0af65958
Chibuike Akpulu <akpulukelvin@gmail.com>
2022-09-07 21:31:57 +0100

    first commit

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'zcoz45w66m';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/prod`
+
+export const authConfig = {
+  domain: 'dev-olkeyhrs.us.auth0.com',
+  clientId: 'sCTC9u1YItkFgrPIzReIud5wWDb2bB3u',
+  callbackUrl: 'http://localhost:3000/callback'
+};
